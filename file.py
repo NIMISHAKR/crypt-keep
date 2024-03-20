e6f27648619a3c451b96f34f94e1dc85f43c0a6c
NIMISHAKR <118978159+NIMISHAKR@users.noreply.github.com>
2024-03-20 20:58:12 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,36 @@
+def encrypt(text, shift):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                encrypted_text += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            else:
+                encrypted_text += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+def decrypt(text, shift):
+    decrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                decrypted_text += chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+            else:
+                decrypted_text += chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+def main():
+    message = input("Enter the message: ")
+    shift = int(input("Enter the shift value: "))
+
+    encrypted_message = encrypt(message, shift)
+    print("Encrypted message:", encrypted_message)
+
+    decrypted_message = decrypt(encrypted_message, shift)
+    print("Decrypted message:", decrypted_message)
+
+if __name__ == "__main__":
+    main()
